d969341578430eca1036619422abc76528e2d989
gend01 <gend00000000@gmail.com>
2021-08-10 09:16:30 +0300

    add css modules

---
+++ src/components/static/Footer/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from './styles.module.css'
+
+
+
+function Footer() {
+ 
+
+
+  return (
+
+    <footer>
+        <h1>Footer component</h1>
+    </footer>
+
+  );
+
+}
+
+export default Footer;
